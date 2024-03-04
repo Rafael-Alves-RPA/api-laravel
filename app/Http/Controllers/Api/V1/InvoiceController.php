f0812cbd6cf765d784dbbf3d45979a2ab5e0871f
Rafael-Alves-RPA <Rafael_palves.ti_@hotmail.com>
2024-03-04 07:46:01 -0300

    Initial commit

---
+++ app/Http/Controllers/Api/V1/InvoiceController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\V1\InvoiceResource;
+use App\Models\Invoice;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class InvoiceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return InvoiceResource::collection(Invoice::with('user')->get());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validator = Validator::make(
+            $request->all(),
+            [
+                'user_id' => 'required',
+                'type' => 'required|max:1',
+                'paid' => 'required|numeric|between:0,1',
+                'payment_date' => 'nullable',
+                'value' => 'required|numeric|between:1, 9999.99',
+
+            ]
+        );
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
